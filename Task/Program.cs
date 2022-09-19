92091baa088c15fe8144a9158a595071ede152e2
Alona Ratsebarska <aleonorarace@gmail.com>
2022-09-19 18:52:30 +0300

    Добавлены метод FillArray, PrintArray. Внесено пару корректировок

--- Task/Program.cs
+++ Task/Program.cs
@@ -7,11 +7,35 @@
 ["Denmark", "France", "Italy"] -> []
 */
 int arraySize = Convert.ToInt32(InputInfo("Введите желаемое количество введённых Вами единиц информации:"));
- int[] array = new int[arraySize];
+
+string[] array = new string[arraySize];
+FillArray(array);
+
+Console.WriteLine($"\n Введённая Вами информация в формате массива: ");
+PrintArray(array);
+
 string InputInfo(string input)
 {
     Console.Write(input);
     string output = Console.ReadLine();
     return output;
 
+}
+
+void FillArray(string[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        array[i] = InputInfo("Введите любое значение (слово, цифру, знак и тд.): ");
+    }
+}
+
+void PrintArray(string[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if(i == 0) Console.Write($"[{array[i]}, ");
+        else if(i > 0 && i < arr.Length -1) Console.Write($"{array[i]}, ");
+        else Console.Write($"{array[i]}]");
+    }
 }
