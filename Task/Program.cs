dce38e7e7fbb90e95ac037fe2e2ca469f47fc363
Alona Ratsebarska <aleonorarace@gmail.com>
2022-09-19 16:53:06 +0300

    Добавлены метод InputInfo и создан массив array

---
+++ Task/Program.cs
@@ -0,0 +1,17 @@
+﻿/*
+Написать программу, которая из имеющегося массива строк формирует массив из строк,
+длина ккоторых меньше либо равна 3 символа.
+Примеры:
+["hello", "2", "world", ":-)"] -> ["2", ":-)"]
+[“1234“, "1567", "-2", "computer science"] -> [“-2“]
+["Denmark", "France", "Italy"] -> []
+*/
+int arraySize = Convert.ToInt32(InputInfo("Введите желаемое количество введённых Вами единиц информации:"));
+ int[] array = new int[arraySize];
+string InputInfo(string input)
+{
+    Console.Write(input);
+    string output = Console.ReadLine();
+    return output;
+
+}
